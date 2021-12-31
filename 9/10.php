4840f7c36fc04f2f72ad3a5bb0184494b91d9bab
Kayra İnce <a.kayraince@icloud.com>
2021-12-31 13:56:08 +0300

    son kankaaaaa

--- 9/10.php
+++ 9/10.php
@@ -1,19 +1,24 @@
-<?php
 
-function iyz_cut($Price, $iyz_variable)
-{
-    $cut = ($Price * ($iyz_variable / 100)) + 0.25;
+
+
+
+
+<?php
+function iyz_cut($Price, $iyz_variable) {
+    $cut = ($Price * ($iyz_variable/100)) + 0.25;
     return $cut;
 }
-function iyzPass($Price, $iyz_variable, $VendorCom) {
 
-    $com_applicable = $Price - iyz_cut($Price, $iyz_variable);
-    return $com_applicable * ($VendorCom/100);
-
-
-
+function art_cut($Price, $iyz_variable, $com_art){
+    $art_profit = ($Price * $com_art/100) - iyz_cut($Price, $iyz_variable);
+    return $art_profit;
 }
-
-iyzPass(100, 2.99,80,18); // call the function
+function iyzicoPass($Price, $iyz_variable, $com_art, $shipping){
+    $vendor_cut = $Price - art_cut($Price, $iyz_variable, $com_art);
+    $iyzi_final = $vendor_cut - $shipping;
+    return $iyzi_final;
+}
+iyzicoPass(100, 2.99,25,15); // call the function
 
 ?>
+
