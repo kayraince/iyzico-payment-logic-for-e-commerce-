7020fd073cc78dc12572687ec9e89956a34a9de9
Kayra İnce <a.kayraince@icloud.com>
2022-01-03 13:52:01 +0300

    10/10

--- 9/10.php
+++ 9/10.php
@@ -4,19 +4,19 @@
 
 
 <?php
-function iyz_cut($Price, $iyz_variable) {
-    $cut = ($Price * ($iyz_variable/100)) + 0.25;
+function iyz_cut($price, $iyz_variable) {
+    $cut = ($price * ($iyz_variable/100)) + 0.25;
     return $cut;
 }
 
-function art_cut($Price, $iyz_variable, $com_art, $VAT){
-    $art_profit = ($Price * $com_art/100) - iyz_cut($Price, $iyz_variable);
+function art_cut($price, $iyz_variable, $com_art, $VAT){
+    $art_profit = ($price * $com_art/100) - iyz_cut($price, $iyz_variable);
     return $art_profit * (1 + ($VAT/100));
 }
-function iyzicoPass($Price, $iyz_variable, $com_art, $shipping, $VAT){
-    $vendor_cut = $Price - art_cut($Price, $iyz_variable, $com_art, $VAT);
+function iyzicoPass($price, $iyz_variable, $com_art, $shipping, $VAT){
+    $vendor_cut = $price - art_cut($price, $iyz_variable, $com_art, $VAT);
     $iyzi_final = $vendor_cut - ($shipping * (1 + ($VAT/100)));
-    echo $iyzi_final;
+    return $iyzi_final;
 }
 
 iyzicoPass(1000, 2.99,25,30,18); // call the function
